0100687353892da7e81d3d968073dad1e0fa5585
Igor Matheus de Souza <igorm1411@gmail.com>
2017-08-28 02:11:52 -0300

    Add files via upload

---
+++ Controllers/JogosController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaJogos.Models;
+using SistemaJogos.ConnectionDB;
+using System.Collections;
+
+namespace SistemaJogos.Controllers
+{
+    public class JogosController : Controller
+    {
+       private sistemaGamesEntities2 db = new sistemaGamesEntities2();
+      
+        public ActionResult Index()
+        {
+            var Model = from mat in db.jogos
+                         select mat;
+            
+            return View(Model);
+        }
+       
+
+        [HttpPost]
+         public ActionResult Index(string nomeJogo){ 
+             var Model = from mat in db.jogos
+                         where mat.nome.Contains(nomeJogo)
+                         select mat;
+
+             return View(Model);
+         
+         }
+       
+
+       
+         
+         
+    }
+}
---
+++ Models/Jogos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SistemaJogos.ConnectionDB;
+
+namespace SistemaJogos.Models
+{
+    public class Jogos
+    {
+
+        sistemaGamesEntities2 connect = new sistemaGamesEntities2();
+        
+        public Jogos()
+        {
+
+        }
+        public jogos SelecionarGamePeloId(int id)
+        {
+            
+            jogos jogo = connect.jogos.First(o => o.idjogos == id);
+            return jogo;
+
+        }
+        public int idjogos { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public string img { get; set; }
+        public Nullable<System.DateTime> dtLancamentos { get; set; }
+        public string plataforma { get; set; }
+        public Nullable<float> preco { get; set; }
+        public Nullable<bool> validacao { get; set; }
+        public Nullable<int> quantidade { get; set; }
+
+        public int SelecionarQuant()
+        {
+            int k = (from mat in connect.jogos
+                     select mat.idjogos).Count();
+
+            return k;
+        }
+
+        public jogos SelecionarGamePeloNome(string nome)
+        {
+            jogos game = new jogos();
+
+            int k = (from mat in connect.jogos
+                     select mat.idjogos).Count();
+
+            for (int i = 0; i < k; i++)
+            {
+                game = connect.jogos.First(pk => pk.idjogos == i);
+
+                if ((game.nome.ToLower().Contains(nome.ToLower())) && game.validacao == true)
+                {
+                    return game;
+                }
+
+
+
+            }
+
+            return game;
+        }
+    }
+}
